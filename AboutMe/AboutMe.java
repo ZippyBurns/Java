c2223c303c73db8869c88a8afc3a3adff16c80b5
ZippyBurns <47673797+ZippyBurns@users.noreply.github.com>
2019-03-30 07:42:33 -0700

    Add files via upload

---
+++ AboutMe/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args){
+        System.out.println("My name is Justin");
+        System.out.println("I'm 29 years old.");
+        System.out.println("My hometown is Camanche, Iowa");
+    }
+}
