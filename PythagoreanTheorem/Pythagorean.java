5ec6015619f795d530914517b9045902fd5a6025
ZippyBurns <47673797+ZippyBurns@users.noreply.github.com>
2019-04-02 11:53:46 -0700

    Add files via upload

---
+++ PythagoreanTheorem/Pythagorean.java
@@ -0,0 +1,8 @@
+import java.awt.geom.Rectangle2D.Double;
+import java.lang.Math;
+public class Pythagorean{
+    public static void main (String[] args) {
+        PythagoreanTest run = new PythagoreanTest();
+        run.calculateHypotenuse(24, 24);
+        }
+    }
---
+++ PythagoreanTheorem/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest{
+    public void calculateHypotenuse(int legA, int legB) {
+        double squareRoot = Math.sqrt((legA * legA) + (legB * legB)); 
+    System.out.println(squareRoot);
+    // return squareRoot;
+    }
+}
