08821f17c207fd2561bd8662005d63f80a3b0e46
ZippyBurns <47673797+ZippyBurns@users.noreply.github.com>
2019-04-11 17:06:55 -0700

    Add files via upload

---
+++ mvc/src/main/java/com/justin/mvc/services/BookService.java
@@ -0,0 +1,59 @@
+package com.justin.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.justin.mvc.models.Book;
+import com.justin.mvc.repositories.BookRepository;
+
+@Service 
+public class BookService {
+	private final BookRepository bookRepository;
+
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	
+	// returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+        
+     }
+
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+	}
+
+	public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+		Optional<Book> update = bookRepository.findById(id);
+		if(update.isPresent()) {
+			Book found = update.get();
+			found.setTitle(title);
+			found.setDescription(desc);
+			found.setLanguage(lang);
+			found.setNumberOfPages(numOfPages);
+			return bookRepository.save(found);
+		}
+		else {
+			return null;
+		}
+    
+    }
+}
